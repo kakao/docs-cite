4daeaf5aaca5c64b9cd6051ed856e308048e6f75
Niko Bellic <niko.bellic@kakaocorp.com>
2016-12-07 19:40:53 +0900

    added download svg link

--- static/javascripts/sequence-diagram-loader.js
+++ static/javascripts/sequence-diagram-loader.js
@@ -14,3 +14,15 @@ $.each($('.sequence_diagram svg'), function() {
     'max-width': '100%'
   });
 });
+
+$('.sequence_diagram_download').click(function() {
+  var target = $(this).attr('target');
+  var svg = $('#' + target + ' svg');
+  var viewBox = svg.attr('viewBox').split(' ');
+  var svgWidth = viewBox[2];
+  var svgHeight = viewBox[3];
+  var xml = '<?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"><svg xmlns="http://www.w3.org/2000/svg" width="' + svgWidth + '" height="' + svgHeight + '" xmlns:xlink="http://www.w3.org/1999/xlink">' + svg.html() + '</svg>';
+
+  $(this).attr("download", target + ".svg");
+  $(this).attr("href", "data:image/svg+xml," + encodeURIComponent(xml));
+});
