40ebc34993585bcfb688bae22564e56c25960ab3
Seung Lee <code0x9@gmail.com>
2016-12-07 17:12:54 +0900

    updating document

--- javascripts/sequence-diagram-loader.js
+++ javascripts/sequence-diagram-loader.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {
-  $.each($('div[type=sequence_diagram]'), function(idx, el) {
-    var id = $(this).attr('id');
-    var dsl = $(this).text();
-
-    $(this).html('');
-    var diagram = Diagram.parse(dsl);
-    diagram.drawSVG(id, {theme: 'simple'});
-  });
-});
+$('.sequence_diagram').sequenceDiagram({theme: 'simple'});
