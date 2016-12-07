fbc65cd34c6551098a8add7fa8227b322d6c39c2
Seung Lee <code0x9@gmail.com>
2016-12-07 18:31:08 +0900

    updating document

--- public/javascripts/sequence-diagram-loader.js
+++ public/javascripts/sequence-diagram-loader.js
@@ -1,1 +1,12 @@
 $('.sequence_diagram').sequenceDiagram({theme: 'simple'});
+
+// resize svg diagrams via viewBox
+$.each($('.sequence_diagram svg'), function() {
+  var width = $(this).attr('width');
+  var height = $(this).attr('height');
+
+  $(this).attr({
+    'viewBox': '0 0 ' + width + ' ' + height,
+    'width': '100%'
+  });
+});
